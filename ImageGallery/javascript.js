66b87f4a265d6cdd793162cab95a0495ae8ef561
Venaktasukumar <123547412+Venaktasukumar@users.noreply.github.com>
2023-03-11 19:59:25 +0530

    Update javascript.js

--- ImageGallery/javascript.js
+++ ImageGallery/javascript.js
@@ -1,12 +1,13 @@
 const lightbox=document.querySelector(".light-box");
 const lightboximages=lightbox.querySelector(".light-box img");
 const container=document.querySelector(".container");
-
-let openfullimage = (obj) =>{
-    lightboximages.src=obj.src; 
+const thumbnailimages=document.querySelectorAll(".images img");
+thumbnailimages.forEach((image) => { image.addEventListener("click", ()=>{
     container.style.opacity="0.4";
+    lightboximages.src=image.src;
     lightbox.style.display="flex";
-}
+})});
+
 let closefullimage = () =>{
     container.style.opacity="1";
     lightbox.style.display="none";
